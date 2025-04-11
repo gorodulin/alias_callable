a035c823d6fc4dd40a0a165d731e7dca30321ee4
Vladimir Gorodulin <ru.hostmaster@gmail.com>
2025-04-11 20:35:58 +0000

    Add backtrace filtering feature

--- lib/alias_callable.rb
+++ lib/alias_callable.rb
@@ -10,12 +10,33 @@ module AliasCallable
   autoload :UnknownCallableError,         "alias_callable/unknown_callable_error"
   autoload :VERSION,                      "alias_callable/version"
 
+  @backtrace_filtering_enabled = false
+
   def self.enable_globally
     ::Module.include(::AliasCallable::ClassMethods) unless enabled_globally?
   end
 
   def self.enabled_globally?
     ::Module.included_modules.include?(::AliasCallable::ClassMethods)
+  end
+
+  def self.enable_backtrace_filtering
+    return if backtrace_filtering_enabled?
+
+    ::Exception.class_eval do
+      alias_method :original_backtrace, :backtrace
+
+      def backtrace
+        bt = original_backtrace
+        bt&.grep_v(%r{/alias_callable/})
+      end
+    end
+
+    @backtrace_filtering_enabled = true
+  end
+
+  def self.backtrace_filtering_enabled?
+    @backtrace_filtering_enabled
   end
 
 end
