f7a222e43b60d0a22984dfcf0e886ad95dd59670
Vladimir Gorodulin <ru.hostmaster@gmail.com>
2025-04-13 13:50:30 +0000

    Spec added

---
+++ spec/acceptance/09_create_alias_in_main_context/09_story.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Story09
+  class DoSomething
+    def self.call
+      puts "Doing something"
+    end
+  end
+end
+
+alias_callable :do_something, Story09::DoSomething
---
+++ spec/acceptance/09_create_alias_in_main_context/09_story_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe "Story09" do
+
+  describe "#alias_callable" do
+    context "when used in the main context" do
+      it "raises error and does not define a new method" do
+        expect(AliasCallable.enabled_globally?).to be_truthy # ensure the feature is enabled
+        expect do
+          load File.join(__dir__, "09_story.rb")
+        end.to raise_error(NoMethodError, /undefined method 'alias_callable' for main/)
+        expect(eval("respond_to?(:do_something, true)", TOPLEVEL_BINDING, __FILE__, __LINE__), __FILE__,
+               __LINE__).to be false
+      end
+    end
+  end
+end
